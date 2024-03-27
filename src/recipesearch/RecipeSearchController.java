e140d0e6cfad54f43c8297cd1534cb197d1b5921
bllnr <52850477+bllnr@users.noreply.github.com>
2024-03-27 16:01:24 +0100

    Add listeners, fix fxml-files

--- src/recipesearch/RecipeSearchController.java
+++ src/recipesearch/RecipeSearchController.java
@@ -35,14 +35,23 @@ public class RecipeSearchController implements Initializable {
     List<Recipe> recipeList;
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+
         updateRecipeList();
+
+        initMainIngredient();
+
+        initCuisine();
+
+        initDifficulty();
+
+        initPrice();
+
+        initTime();
     }
 
     private void updateRecipeList(){
         //Empty the flowpane
-        if(recipeListFlowPane != null){
-            recipeListFlowPane.getChildren().clear();
-        }
+        recipeListFlowPane.getChildren().clear();
 
         //Anropa recipeBackendController för att hämta listan på alla recept (osorterad)
         recipeList = recipeBackendController.getRecipes();
@@ -53,7 +62,94 @@ public class RecipeSearchController implements Initializable {
             recipeListFlowPane.getChildren().add(new RecipeListItem(
                     recipeList.get(i), this));
         }
-
     }
+
+    private void initMainIngredient() {
+        //Set available items
+        inputIngredient.getItems().addAll("Välj huvudingrediens", "Kött", "Fisk", "Kyckling", "Vegetarisk");
+        //Choose default alternative to display
+        inputIngredient.getSelectionModel().select("Välj huvudingrediens");
+
+        inputIngredient.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                recipeBackendController.setMainIngredient(newValue);
+                updateRecipeList();
+            }
+        });
+    }
+
+    private void initCuisine() {
+        inputCuisine.getItems().addAll("Välj kök", "Sverige", "Grekland", "Indien", "Asien", "Afrika", "Frankrike");
+        inputCuisine.getSelectionModel().select("Välj kök");
+
+        inputCuisine.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                recipeBackendController.setMainIngredient(newValue);
+                updateRecipeList();
+            }
+        });
+    }
+
+    private void initDifficulty() {
+        //Combine all radio-buttons to one toggle group
+        ToggleGroup difficultyToggleGroup = new ToggleGroup();
+        inputDifficultyAll.setToggleGroup(difficultyToggleGroup);
+        inputDifficultyEasy.setToggleGroup(difficultyToggleGroup);
+        inputDifficultyMedium.setToggleGroup(difficultyToggleGroup);
+        inputDifficultyHard.setToggleGroup(difficultyToggleGroup);
+        //Default:
+        inputDifficultyAll.setSelected(true);
+
+        // Add listener to update backend when radio buttons are toggled
+        difficultyToggleGroup.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+            @Override
+            public void changed(ObservableValue<? extends Toggle> observable, Toggle oldValue, Toggle newValue) {
+
+                if (difficultyToggleGroup.getSelectedToggle() != null) {
+                    RadioButton selected = (RadioButton) difficultyToggleGroup.getSelectedToggle();
+                    recipeBackendController.setDifficulty(selected.getText());
+                    updateRecipeList();
+                }
+            }
+        });
+    }
+
+    private void initPrice(){
+        SpinnerValueFactory<Integer> valueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 10000, 100, 10);
+
+        inputMaxPrice.setValueFactory(valueFactory);
+
+        inputMaxPrice.valueProperty().addListener(new ChangeListener<Integer>(){
+
+            @Override
+            public void changed(ObservableValue<? extends Integer> observableValue, Integer integer, Integer newValue) {
+                recipeBackendController.setMaxPrice(newValue);
+                updateRecipeList();
+            }
+        });
+
+        inputMaxPrice.focusedProperty().addListener(new ChangeListener<Boolean>() {
+
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+
+                if(newValue){
+                    //focusgained - do nothing
+                }
+                else{
+                    Integer value = Integer.valueOf(inputMaxPrice.getEditor().getText());
+                    recipeBackendController.setMaxPrice(value);
+                    updateRecipeList();
+                }
+
+            }
+        });
+    }
+
+    private void initTime(){}
 
 }
